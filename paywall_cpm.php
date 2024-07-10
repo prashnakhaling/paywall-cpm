05b81b1a607c984ca05e38985b989ef3b2fa2a42
ranju58 <ranjubhusal01@gmail.com>
2024-07-10 10:21:37 +0545

    new

--- paywall_cpm.php
+++ paywall_cpm.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Paywall
-Description: A plugin where use cna log in and read article on the basis of the credits remaining in their account.
+Description: A plugin where use can log in and read article on the basis of the credits remaining in their account.
 Version: 1.0
 Author: CPM
 License: GPL2
