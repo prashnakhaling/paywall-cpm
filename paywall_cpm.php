dfab8cfac78847934b6fa4c6a76f2e6245338770
prashnakhaling <prashna.krai@gmail.com>
2024-07-10 10:16:39 +0545

    added main file for plugin
    create plugin main file

---
+++ paywall_cpm.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name: Paywall
+Description: A plugin where use cna log in and read article on the basis of the credits remaining in their account.
+Version: 1.0
+Author: CPM
+License: GPL2
+Text Domain: paywall_cpm
+*/
