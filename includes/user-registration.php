d1aac7d1b3e2d2d41adbd2a55aae6831d7890c4a
ranju58 <ranjubhusal01@gmail.com>
2024-07-11 14:20:26 +0545

    user registration and credit added

--- includes/user-registration.php
+++ includes/user-registration.php
@@ -1,37 +1,24 @@
 <?php
-// Handle registration form submission
-function paywall_cpm_handle_registration()
+function paywall_handle_registration()
 {
-    if (isset($_POST['paywall_cpm_register_nonce']) && wp_verify_nonce($_POST['paywall_cpm_register_nonce'], 'paywall_cpm_register')) {
-        $username = sanitize_user($_POST['username']);
-        $email = sanitize_email($_POST['email']);
-        $password = sanitize_text_field($_POST['password']);
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['paywall_register_nonce']) && wp_verify_nonce($_POST['paywall_register_nonce'], 'paywall_register')) {
+            $username = sanitize_user($_POST['username']);
+            $email = sanitize_email($_POST['email']);
+            $password = $_POST['password'];
 
-        // Validate username and email
-        if (!username_exists($username) && !email_exists($email)) {
             $user_id = wp_create_user($username, $password, $email);
 
-            if (!is_wp_error($user_id)) {
-                // Optionally login the user after registration
-                $user = get_user_by('id', $user_id);
-                wp_set_current_user($user_id, $user->user_login);
-                wp_set_auth_cookie($user_id);
-                do_action('wp_login', $user->user_login);
-
-                // Give user initial credits
-                update_user_meta($user_id, 'credits', 5);
-
-                wp_redirect(home_url('/login?registered=1'));
-                exit;
+            if (is_wp_error($user_id)) {
+                // Registration failed
+                wp_redirect(home_url('/register?error=1'));
             } else {
-                wp_redirect(home_url('/register?error=registration_failed'));
-                exit;
+                // Registration successful
+                add_user_meta($user_id, 'paywall_credits', 5, true);
+                wp_redirect(home_url('/login?registered=1'));
             }
-        } else {
-            wp_redirect(home_url('/register?error=username_email_exists'));
             exit;
         }
     }
 }
-add_action('admin_post_nopriv_paywall_cpm_register', 'paywall_cpm_handle_registration');
-add_action('admin_post_paywall_cpm_register', 'paywall_cpm_handle_registration');
+add_action('init', 'paywall_handle_registration');
